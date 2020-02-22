814f5a2bfedeedcb6f3eed63feb43f91e3b9eafc
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2020-02-22 02:55:57 -0600

    Disabled RCON
    Due to some issues with rcon, currently find a better method to implement it/write it properly, most likely moving to Vaadin 14, but want to keep it's support.

--- src/main/java/me/wilsonhu/ozzie/Application.java
+++ src/main/java/me/wilsonhu/ozzie/Application.java
@@ -5,7 +5,6 @@ public class Application {
     public static void main(String[] args) throws Exception {
         Ozzie ozzie = new Ozzie(args);
         ozzie.start();
-        ozzie.getRConServer().execute();
-        //System.out.println(Locale.getDefault().toString());//Use this v:
+        //ozzie.getRConServer().execute();
     }
 }
